5339980192153b1b14dc4b6203344882d1817b2a
Martin Gontovnikas <martin@gonto.com.ar>
2013-04-04 01:10:43 -0300

    Added Node files

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/**\n' +
+      ' * <%= pkg.description %>\n' +
+      ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * @link <%= pkg.homepage %>\n' +
+      ' * @author <%= pkg.author %>\n' +
+      ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
+      ' */\n'
+    },
+    dirs: {
+      dest: 'dist'
+    },
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['src/*.js''],
+        dest: '<%= dirs.dest %>/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['<%= concat.dist.dest %>'],
+        dest: '<%= dirs.dest %>/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js'],
+      options: {
+        curly: false,
+        browser: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        expr: true,
+        node: true,
+        globals: {
+          exports: true,
+          angular: false,
+          $: false
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Load the plugin that provides the "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+  // Default task.
+  grunt.registerTask('default', ['build']);
+
+  // Build task.
+  grunt.registerTask('build', ['concat', 'uglify']);
+
+
+  // Provides the "bump" task.
+  grunt.registerTask('bump', 'Increment version number', function() {
+    var versionType = grunt.option('type');
+    function bumpVersion(version, versionType) {
+      var type = {patch: 2, minor: 1, major: 0},
+          parts = version.split('.'),
+          idx = type[versionType || 'patch'];
+      parts[idx] = parseInt(parts[idx], 10) + 1;
+      while(++idx < parts.length) { parts[idx] = 0; }
+      return parts.join('.');
+    }
+    var version;
+    function updateFile(file) {
+      var json = grunt.file.readJSON(file);
+      version = json.version = bumpVersion(json.version, versionType || 'patch');
+      grunt.file.write(file, JSON.stringify(json, null, '  '));
+    }
+    updateFile('package.json');
+    updateFile('component.json');
+    grunt.log.ok('Version bumped to ' + version);
+  });
+
+};
