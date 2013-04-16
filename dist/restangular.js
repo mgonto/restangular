9ce9e6e9154b1f0ca040f8ee20deddf5d76748eb
Martin Gontovnikas <martin@gonto.com.ar>
2013-04-16 15:57:01 -0300

    Updated package.json to conform to cdnjs

--- dist/restangular.js
+++ dist/restangular.js
@@ -1,6 +1,6 @@
 /**
  * Restfull Resources service for AngularJS apps
- * @version v0.4.2 - 2013-04-15
+ * @version v0.4.2 - 2013-04-16
  * @link https://github.com/mgonto/restangular
  * @author Martin Gontovnikas <martin@gonto.com.ar>
  * @license MIT License, http://www.opensource.org/licenses/MIT
