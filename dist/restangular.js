839e8e6f95a5cc8ea6f16f42316e6d4303fbc348
Martin Gontovnikas <martin@gonto.com.ar>
2013-05-23 18:45:52 -0300

    Fix for latest version

--- dist/restangular.js
+++ dist/restangular.js
@@ -1,6 +1,6 @@
 /**
  * Restfull Resources service for AngularJS apps
- * @version v0.6.7 - 2013-05-23
+ * @version v0.6.8 - 2013-05-23
  * @link https://github.com/mgonto/restangular
  * @author Martin Gontovnikas <martin@gonto.com.ar>
  * @license MIT License, http://www.opensource.org/licenses/MIT
