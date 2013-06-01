0ff32bc43dff646791aec726a485cd9e60ed56e7
Martin Gontovnikas <martin@gonto.com.ar>
2013-06-01 03:57:42 -0300

    Changed component.json to bower.json

--- dist/restangular.js
+++ dist/restangular.js
@@ -1,6 +1,6 @@
 /**
  * Restfull Resources service for AngularJS apps
- * @version v0.7.3 - 2013-05-31
+ * @version v0.7.3 - 2013-06-01
  * @link https://github.com/mgonto/restangular
  * @author Martin Gontovnikas <martin@gonto.com.ar>
  * @license MIT License, http://www.opensource.org/licenses/MIT
