f3cbd451af4ec7fdd9acc51d4005a9ccd0818691
Leonti Bielski <lbielski@testobject.com>
2013-06-28 12:23:36 +0200

    #146 Explicitly check if element id is null or undefined

--- src/restangular.js
+++ src/restangular.js
@@ -373,7 +373,7 @@ module.provider('Restangular', function() {
                     
                     if (!elem[__this.config.restangularFields.restangularCollection]) {
                         var elemId = __this.config.getIdFromElem(elem);
-                        if (elemId) {
+                        if (!_.isUndefined(elemId) && !_.isNull(elemId)) {
                             currUrl += "/" + elemId;
                         }
                     }
