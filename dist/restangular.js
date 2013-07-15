2dbc0a111936bec6f2d174f6a7297061ff98f56d
Chris Boden <chris@binnj.com>
2013-07-15 11:17:39 -0400

    Re-adjusted single element check

--- dist/restangular.js
+++ dist/restangular.js
@@ -399,7 +399,7 @@ module.provider('Restangular', function() {
                     
                     if (!elem[__this.config.restangularFields.restangularCollection]) {
                         var elemId = __this.config.getIdFromElem(elem);
-                        if (elemId) {
+                        if ("" !== elemId && !_.isUndefined(elemId) && !_.isNull(elemId)) {
                             currUrl += "/" + elemId;
                         }
                     }
