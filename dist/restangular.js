f15d17adfac54bfc9bf0d6180baa04c89fcb0909
Chris Boden <chris@binnj.com>
2013-07-16 10:18:10 -0400

    grunt generation

--- dist/restangular.js
+++ dist/restangular.js
@@ -1,6 +1,6 @@
 /**
  * Restfull Resources service for AngularJS apps
- * @version v1.0.7 - 2013-07-15
+ * @version v1.0.7 - 2013-07-16
  * @link https://github.com/mgonto/restangular
  * @author Martin Gontovnikas <martin@gonto.com.ar>
  * @license MIT License, http://www.opensource.org/licenses/MIT
