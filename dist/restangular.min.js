9ef0483d8d7460a8db4b3515d2ab5df2391e1fdb
Martin Gontovnikas <martin@gonto.com.ar>
2013-11-10 17:37:46 -0300

    1.1.8

--- dist/restangular.min.js
+++ dist/restangular.min.js
@@ -1,6 +1,6 @@
 /**
  * Restful Resources service for AngularJS apps
- * @version v1.1.7 - 2013-11-10
+ * @version v1.1.8 - 2013-11-10
  * @link https://github.com/mgonto/restangular
  * @author Martin Gontovnikas <martin@gonto.com.ar>
  * @license MIT License, http://www.opensource.org/licenses/MIT
