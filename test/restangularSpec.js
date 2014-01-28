e3eef7a3ff9dd8b7c089895c7b8003bbe3dc7034
James Allardice <jim.allardice@gmail.com>
2014-01-28 11:46:40 +0000

    Test for existing self-linking functionality (following the HAL model)

--- test/restangularSpec.js
+++ test/restangularSpec.js
@@ -2,6 +2,7 @@ describe("Restangular", function() {
   // API
   var Restangular, $httpBackend;
   var accountsModel, restangularAccounts, restangularAccount0, restangularAccount1;
+  var accountsHalModel;
   var messages, newAccount;
 
   // Utils
@@ -33,6 +34,15 @@ describe("Restangular", function() {
       {id: 1, user: "Paul", amount: 3.1416, transactions: [{from: "Martin", amount: 3, id: 0}, {from: "Anonymous", amount: 0.1416, id:1}]}
     ];
 
+    // HAL model (http://stateless.co/hal_specification.html)
+    accountsHalModel = [
+      {id: 0, user: "Martin", amount: 42, transaction: [], _links: {self: "/accountsHAL/martin"}},
+      {id: 1, user: "Paul", amount: 3.1416, transaction: [
+        {from: "Martin", amount: 3, id: 0, _links: {self: "/accountsHAL/paul/transactions/0"}},
+        {from: "Anonymous", amount: 0.1416, id: 1, _links: {self: "/accountsHAL/paul/transactions/1"}}
+      ], _links: {self: "/accountsHAL/paul"}}
+    ];
+
     newAccount = {id: 44, user: "First User", amount: 45, transactions: []};
 
     messages = [{id: 23, name: "Gonto"}, {id: 45, name: "John"}]
@@ -53,6 +63,12 @@ describe("Restangular", function() {
     $httpBackend.whenGET("/accounts/1").respond(accountsModel[1]);
     $httpBackend.whenGET("/accounts/1/transactions").respond(accountsModel[1].transactions);
     $httpBackend.whenGET("/accounts/1/transactions/1").respond(accountsModel[1].transactions[1]);
+
+    $httpBackend.whenGET("/accountsHAL").respond(accountsHalModel);
+    $httpBackend.whenPUT("/accountsHAL/martin").respond(function(method, url, data) {
+      accountsHalModel[0] = angular.fromJson(data);
+      return [200, data, ""];
+    });
 
     // Full URL
     $httpBackend.whenGET('http://accounts.com/all').respond(accountsModel);
@@ -545,6 +561,26 @@ describe("Restangular", function() {
 
       expect(grandchildRestangular.configuration.baseUrl).toEqual('/api/v1');
       expect(grandchildRestangular.configuration.suffix).toEqual('.json');
+    });
+  });
+
+  describe("Self linking", function() {
+    it("Should request the link in HAL format", function() {
+      var linkRestangular = Restangular.withConfig(function(RestangularConfigurer) {
+        RestangularConfigurer.setRestangularFields({
+          selfLink: "_links.self"
+        });
+      });
+
+      var arr = linkRestangular.all('accountsHAL').getList().$object;
+      $httpBackend.flush();
+
+      var account = arr[0];
+      $httpBackend.expectPUT("/accountsHAL/martin");
+      account.name = "Updated";
+      account.put();
+
+      $httpBackend.flush();
     });
   });
 });
